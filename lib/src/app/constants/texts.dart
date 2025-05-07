b10ff898bfe74ce4cd8512248c35609da3645412
diep312 <diepnh0312002@gmail.com>
2025-05-07 21:38:43 +0700

    [CHATCHIT-2] Added authentication

---
+++ lib/src/app/constants/texts.dart
@@ -0,0 +1,37 @@
+class DefaultTexts {
+  // Navigation Bar
+  static String get home => 'Trang chủ';
+  static String get favs => 'Yêu thích';
+  static String get chats => 'Trò chuyện';
+  static String get users => 'Người dùng';
+  static String get okay => 'Đồng ý';
+
+  static String get likes => 'Thích';
+  static String get noInternetConnectionHeader => 'Lỗi kết nối';
+  static String get noInternetConnectionContent =>
+      ' Không có kết nối internet.\nVui lòng kiểm tra và thử lại sau.';
+  static String get typePhoneNumber => 'Nhập số điện thoại';
+  static String get invalidPhoneNumber => 'Số điện thoại không hợp lệ';
+  static String get typeAtLeastTwoLetters => 'Nhập ít nhất hai ký tự.';
+  static String get continuee => 'Tiếp tục';
+  static String get enterVerificationCode =>
+      'Nhập mã xác thực đã được gửi đến điện thoại của bạn';
+  static String get securityCode => 'Mã bảo mật';
+  static String get remainingTime => 'Thời gian còn lại';
+  static String get sendCodeAgain => 'Gửi lại mã';
+  static String get typePersonalInfos =>
+      'Nhập thông tin cá nhân để thiết lập tài khoản của bạn';
+  static String get yourFirstName => 'Tên của bạn';
+  static String get yourLastName => 'Họ của bạn';
+  static String get typeYourFirstName => 'Nhập tên của bạn';
+  static String get typeYourLastName => 'Nhập họ của bạn';
+  static String get signUp => 'Đăng ký';
+
+  static String get genericErrorHeader => 'Đã xảy ra lỗi!';
+  static String get genericErrorContent =>
+      'Đã xảy ra lỗi không mong muốn.\nVui lòng thử lại sau.';
+  static String get tooManyRequests =>
+      'Quá nhiều yêu cầu.\nVui lòng thử lại sau.';
+  static String get wrongVerificationCode =>
+      'Mã xác thực không đúng. Vui lòng đảm bảo bạn nhập đúng các số.';
+}
