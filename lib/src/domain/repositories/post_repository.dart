a26b75b4f3abc33c55309975a0ff6af2c3775a96
NguyenPhong2612 <nguyendacphong0307@gmail.com>
2025-05-09 23:43:11 +0700

    add get post in post reporitory

--- lib/src/domain/repositories/post_repository.dart
+++ lib/src/domain/repositories/post_repository.dart
@@ -1,7 +1,16 @@
+import 'dart:collection';
+
 import 'package:chit_chat/src/domain/entities/post.dart';
+import 'package:chit_chat/src/domain/entities/user.dart';
 
 abstract class PostRepository {
   void killInstance();
 
   Future<void> addPost(Post post);
+  Future<void> likePost(String postId);
+  Future<void> cancelPostLike(
+    String postId,
+  );
+  Future<void> getNextPosts();
+  Stream<UnmodifiableListView<Post>?> getPosts(User user);
 }
