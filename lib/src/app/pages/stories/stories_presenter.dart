0e2c75863b262197bfc497b5a44c851c4a74deb0
dungnt <iamleader157@gmail.com>
2025-05-09 20:16:52 +0700

    add list stories view

---
+++ lib/src/app/pages/stories/stories_presenter.dart
@@ -0,0 +1,31 @@
+import 'dart:collection';
+
+import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
+import 'package:chit_chat/src/domain/entities/story.dart';
+import 'package:chit_chat/src/domain/repositories/story_repository.dart';
+import 'package:chit_chat/src/domain/repositories/user_repository.dart';
+import 'package:chit_chat/src/domain/usecases/get_stories.dart';
+
+class StoriesPresenter extends Presenter {
+  late Function getStoriesOnNext;
+  late Function getStoriesOnError;
+
+  GetStories _getStories;
+
+  StoriesPresenter(
+    StoryRepository storyRepository,
+    UserRepository userRepository,
+  ) : _getStories = GetStories(
+          storyRepository,
+          userRepository,
+        );
+
+  void getStories() {
+   
+  }
+
+  @override
+  void dispose() {
+    _getStories.dispose();
+  }
+}
