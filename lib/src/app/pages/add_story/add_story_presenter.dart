73b1c80a98081e01254dae25183cc3f8a8c376e0
Thu Hien <139684499+hinnie415@users.noreply.github.com>
2025-05-12 21:40:53 +0700

    update add story controller

--- lib/src/app/pages/add_story/add_story_presenter.dart
+++ lib/src/app/pages/add_story/add_story_presenter.dart
@@ -1,5 +1,8 @@
 import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
 import 'package:chit_chat/src/domain/entities/story_item.dart';
+import 'package:chit_chat/src/domain/repositories/story_repository.dart';
+import 'package:chit_chat/src/domain/repositories/user_repository.dart';
+import 'package:chit_chat/src/domain/usecases/add_story.dart';
 
 class AddStoryPresenter extends Presenter {
   late Function addStoryOnComplete;
@@ -7,14 +10,42 @@ class AddStoryPresenter extends Presenter {
 
   final AddStory _addStory;
 
-  AddStoryPresenter() : _addStory = AddStory();
+  AddStoryPresenter(
+      StoryRepository storyRepository,
+      UserRepository userRepository,
+      ) : _addStory = AddStory(
+    storyRepository,
+    userRepository,
+  );
 
   void addStory(StoryItem storyItem) {
-
+    _addStory.execute(
+      _AddStoryObserver(this),
+      AddStoryParams(storyItem),
+    );
   }
 
   @override
   void dispose() {
     _addStory.dispose();
   }
+}
+
+class _AddStoryObserver extends Observer<void> {
+  final AddStoryPresenter _presenter;
+
+  _AddStoryObserver(this._presenter);
+
+  @override
+  void onComplete() {
+    _presenter.addStoryOnComplete();
+  }
+
+  @override
+  void onError(e) {
+    _presenter.addStoryOnError(e);
+  }
+
+  @override
+  void onNext(_) {}
 }
